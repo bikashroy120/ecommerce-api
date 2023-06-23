a059656dd7e924ffca9fc04b52b5715d00904bde
bikashroy120 <bikashroydt@gmail.com>
2023-06-23 16:23:49 +0600

    add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dbConnect = require("./config/dbConnect");
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const { notFound, errorHandeler } = require("./middlewarer/errorHandelere");
+const { notFound, errorHandler } = require("./middleware/errorHandler");
 const app = express()
 const path = require("path")
 const dotenv = require("dotenv").config();
@@ -16,31 +16,35 @@ const productCatRoutes = require("./routes/productCatRoutes");
 const colorRoutes = require("./routes/colorRoutes");
 const brandRoutes = require("./routes/brandRoutes");
 const blogCatRoutes = require("./routes/blogCatRoutes");
-const couponRoutes = require("./routes/couponRouters");
+const couponRoutes = require("./routes/couponRoutes");
 const uploadImage = require("./routes/uploadRoute")
-dbConnect()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(express.static(path.join(__dirname,"uploads")))
-app.use('/uploads', express.static(__dirname+'/uploads'));
-app.use(cookieParser())
-app.use(morgan('dev'))
 
-app.get("/",(req,res)=>{
-    res.send("Hellow routs")
-})
+dbConnect();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "uploads")));
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+    res.send("Hello routes");
+});
+
 app.use("/api/user", authRoutes);
 app.use("/api/product", productRoutes);
-app.use("/api/blog",blogRoutes);
-app.use("/api/product-category",productCatRoutes);
-app.use("/api/color",colorRoutes);    
-app.use("/api/brand",brandRoutes);
-app.use("/api/blog-category",blogCatRoutes)
-app.use("/api/coupon",couponRoutes)
-app.use("/api/upload",uploadImage) 
-app.use(notFound)
-app.use(errorHandeler)
-app.listen(PORT,()=>{
-    console.log(`Server is running at PORT ${PORT}`)
-})
+app.use("/api/blog", blogRoutes);
+app.use("/api/product-category", productCatRoutes);
+app.use("/api/color", colorRoutes);
+app.use("/api/brand", brandRoutes);
+app.use("/api/blog-category", blogCatRoutes);
+app.use("/api/coupon", couponRoutes);
+app.use("/api/upload", uploadImage);
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`);
+});
